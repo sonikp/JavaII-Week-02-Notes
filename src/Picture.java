6739ff861262c3141b9550748938389f8be5f9ea
Michael.Floerchinger <michael.floerchinger@am.sony.com>
2016-09-29 19:24:11 -0700

    finished mac

--- src/Picture.java
+++ src/Picture.java
@@ -190,6 +190,7 @@ public class Picture extends SimplePicture
   public void mirrorTemple()
   {
 	  /*
+	   * Program 22: copy a section of the temple roof
 	   * startTop x, y = 13, 27
 	   * endtop   x, y = 276,27
 	   * startBottom x, y = 13, 97
@@ -200,24 +201,115 @@ public class Picture extends SimplePicture
 	  int mirrorPoint = 276;
 	  Pixel leftPixel = null;
 	  Pixel rightPixel = null;
+	  int count = 0;
 	  
 	  // loop through row
-	  for (int y = 27; y < 28; y++)	// to 97
+	  for (int y = 27; y < 97; y++)	// to 97
 	  {
 		  // loop through column
-		  for (int x = 13; x < 17; x++)	// to 276
+		  for (int x = 13; x < mirrorPoint; x++)	// to 276
 		  {
-			  System.out.println("Copying clor from " + x + " to " + (mirrorPoint + (mirrorPoint - x)));
+			  //System.out.println("Copying color from " + x + " to " + (mirrorPoint + (mirrorPoint - x)));
 			  leftPixel = getPixel(x, y);
 			  rightPixel = getPixel(mirrorPoint + (mirrorPoint - x), y);
 			  rightPixel.setColor(leftPixel.getColor());
 			  count = count + 1;
 		  }
 	  }
+	  System.out.println("Copied " + count + " pixels");
+  }
+  
+  /*
+   * Program 23: Copying a Picture to the Current Picture
+   * 
+   * Method to copy the picture of Katie to the upper left 
+   * corner of the current picture
+   * 
+   */
+  
+  public void copyKatie()
+  {
+	String sourceFile = "/Users/mfloerchinger/Documents/z.JavaProgramming/UCSD/Java II/CourseCD/mediasources/KatieFancy.jpg";
+	Picture sourcePicture = new Picture(sourceFile);
+	Pixel sourcePixel = null;
+	Pixel targetPixel = null;
+	
+	// loop through the columns
+	for ( int sourceX = 0, targetX = 0; sourceX < sourcePicture.getWidth(); sourceX++, targetX++)		
+	{
+		// loop through the rows
+		for ( int sourceY = 0, targetY = 0; sourceY < sourcePicture.getHeight(); sourceY++, targetY++)		
+		{
+			// set target pixel color to the source pixel color
+			
+			sourcePixel = sourcePicture.getPixel(sourceX, sourceY);
+			targetPixel = this.getPixel(targetX, targetY);
+			targetPixel.setColor(sourcePixel.getColor());
+			
+		}
+	}
+		
   }
   
   
+  /*
+   * Program 25: Copying a Picture to the Current Picture
+   * 
+   * Method to copy the picture of Katie to the upper left 
+   * corner of the current picture
+   * 
+   */
   
+  public void copyKatiesFace()
+  {
+	String sourceFile = "/Users/mfloerchinger/Documents/z.JavaProgramming/UCSD/Java II/CourseCD/mediasources/KatieFancy.jpg";
+	Picture sourcePicture = new Picture(sourceFile);
+	Pixel sourcePixel = null;
+	Pixel targetPixel = null;
+	
+	// loop through the columns
+	for ( int sourceX = 70, targetX = 100; sourceX < 135; sourceX++, targetX++)		// sourcePicture.getWidth()
+	{
+		// loop through the rows
+		for ( int sourceY = 3, targetY = 100; sourceY < 80; sourceY++, targetY++)		// sourcePicture.getHeight()
+		{
+			// set target pixel color to the source pixel color
+			
+			sourcePixel = sourcePicture.getPixel(sourceX, sourceY);
+			targetPixel = this.getPixel(targetX, targetY);
+			targetPixel.setColor(sourcePixel.getColor());
+			
+		}
+	}
+		
+  }
+  
+  /*
+   * Program 30: Scaling down a picture (Smaller)
+   * 
+   */
+  public void copyJakitaSmaller()
+  {
+	  String filePic = "/Users/mfloerchinger/Documents/z.JavaProgramming/UCSD/Java II/CourseCD/mediasources/katie.jpg";
+	  Picture jakitaPicture = new Picture(filePic);
+	  System.out.println(jakitaPicture);
+	  Pixel sourcePixel = null;
+	  Pixel targetPixel = null;
+	  
+	  // loop through columns
+	  for ( int sourceX = 0, targetX = 0; sourceX < jakitaPicture.getWidth(); sourceX+=2, targetX++ )
+	  {
+		  //loop through rows
+		  for ( int sourceY = 0, targetY = 0; sourceY < jakitaPicture.getHeight(); sourceY+=2, targetY++ )
+		  {
+			  sourcePixel = jakitaPicture.getPixel(sourceX, sourceY);
+			  targetPixel = this.getPixel(targetX, targetY);
+			  targetPixel.setColor(sourcePixel.getColor());
+		  }
+	  }
+	  
+	  		
+  }
   
   /**
    * Method to return a string with information about this picture.
@@ -235,19 +327,72 @@ public class Picture extends SimplePicture
  
   public static void main(String[] args) 
   {
-     //String fileName = FileChooser.pickAFile();
-	 //String fileName = "/home/notroot/Java/JavaII/AdditionalSoftware/mediasources/caterpillar.jpg";
-     String fileName = "/home/notroot/Java/JavaII/AdditionalSoftware/mediasources/temple.jpg";
-     Picture pictObj = new Picture(fileName);
-     pictObj.explore();
-     //pictObj.mirrorHorizontalBottomToTop();		// program 20
-     //pictObj.repaint();
+
+	 
+     
+	 //String fileName = "/Users/mfloerchinger/Documents/z.JavaProgramming/UCSD/Java II/CourseCD/mediasources/temple.jpg";
+	 //String fileName = FileChooser.getMediaPath("7inX95in.jpg");
+     //String fileName = "/Users/mfloerchinger/Documents/z.JavaProgramming/UCSD/Java II/CourseCD/mediasources/7inX95in.jpg";
+	 
+	  String fileName = "/Users/mfloerchinger/Documents/z.JavaProgramming/UCSD/Java II/CourseCD/mediasources/katie.jpg";
+	  System.out.println(fileName);
+	  Picture picFile = new Picture(fileName);
+	  System.out.println(picFile);
+	  picFile.copyJakitaSmaller();	// program 30
+	  picFile.show();
+	  
+	  
+	  
+	  /*
+	  String filePic = "/Users/mfloerchinger/Documents/z.JavaProgramming/UCSD/Java II/CourseCD/mediasources/katie.jpg";
+	  Picture jakitaPicture = new Picture(filePic);
+	  jakitaPicture.show();
+	  */
+	  
+	/*
+	 String fileName = "/Users/mfloerchinger/Documents/z.JavaProgramming/UCSD/Java II/CourseCD/mediasources/7inX95in.jpg";
+	 Picture pictObj = new Picture(fileName);
+     pictObj.show();
+     */
+	  
+     /*
+     pictObj.copyJakitaSmaller();		// program 22
+     pictObj.repaint();
+     */
+
      
      /*
      pictObj.lighten2();			// program 18
      pictObj.mirrorVertical();		// program 19
      pictObj.mirrorHorizontal();	// program 20
+     pictObj.mirrorHorizontalBottomToTop();		// program 21
+     pictObj.mirrorTemple();		// program 22
+     pictObj.copyKatie();		// program 23
+     pictObj.copyKatiesFace();		// program 25
+     picFile.copyJakitaSmaller();	// program 30
+
      pictObj.explore();
+     */
+     
+     /*
+     // random crazy notes"
+          //String fileName = FileChooser.pickAFile();
+	 //String fileName = "/home/notroot/Java/JavaII/AdditionalSoftware/mediasources/caterpillar.jpg";
+     //String fileName = "/Users/mfloerchinger/Documents/z.JavaProgramming/UCSD/Java II/CourseCD/mediasources/caterpillar.jpg";
+     // String fileName = "/Users/mfloerchinger/Documents/z.JavaProgramming/UCSD/Java II/CourseCD/mediasources/caterpillar.jpg";
+	  
+	  
+	  //String fileName = FileChooser.pickAFile();
+	  //System.out.println(fileName);
+	  
+	  //System.out.println(FileChooser.pickAFile());
+	  //"//Users/mfloerchinger/Documents/z.JavaProgramming/UCSD/Java II/Assignments/wk02/"
+
+	  // gets media path for a filename
+	  //System.out.println(FileChooser.getMediaPath("temple.jpg"));
+      
+	  // sets media path, I guess for saving
+	  //FileChooser.setMediaPath("/Users/mfloerchinger/Documents/z.JavaProgramming/UCSD/Java II/Assignments/wk02/");
      */
   }
   
